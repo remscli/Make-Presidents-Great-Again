52f05645f968976e16ced2bb6522a6baf70d2030
Rémy <remy@sleede.com>
2016-09-29 13:42:25 +0200

    animate home page entrance

--- client/app/views/Home.js
+++ client/app/views/Home.js
@@ -24,25 +24,35 @@ define(['jquery',
         this.$el.html(this.template());
 
         this.border = this.$el.find('.home-border');
+        this.titleEl = this.$el.find('.home__title');
+        this.starsEl = this.$el.find('.home__stars');
+        this.barsEls = this.$el.find('.home__bars');
 
         this.ready();
       },
 
+      shown: function () {
+        var tl = new TimelineMax({onComplete: function () {
+          $('.home').addClass('home--shown');
+        }});
+        tl.fromTo(this.starsEl, .3, {opacity: 0, y: 20}, {opacity: 1, y: 0}, .8);
+        tl.fromTo(this.titleEl, .3, {opacity: 0, y: 20}, {opacity: 1, y: 0});
+        tl.fromTo('.home__button', .3, {opacity: 0, y: 20}, {opacity: 1, y: 0});
+      },
+
       hide: function () {
-        var homeTitleEl = this.$el.find('.home__title');
-        var homeStarsEl = this.$el.find('.home__stars');
         var homeButtonWrpEl = this.$el.find('.home__button-wrapper');
         var tl = new TimelineMax({onComplete: onTimelineCompleted.bind(this)});
 
         // Give a fixed height to bars
-        $('.home__bars').each(function () {
+        this.barsEls.each(function () {
           $(this).height($(this).height());
         });
 
         tl.to('.home-border', .25, {opacity: 0});
-        tl.to([homeStarsEl, homeButtonWrpEl], .25, {opacity: 0}, 0);
-        tl.to([homeStarsEl, homeButtonWrpEl], .3, {width: 0, padding: 0}, '-=0.05');
-        tl.to([homeTitleEl, '.home__bars'], .3, {height: 0, margin: 0});
+        tl.to([this.starsEl, homeButtonWrpEl], .25, {opacity: 0}, 0);
+        tl.to([this.starsEl, homeButtonWrpEl], .3, {width: 0, padding: 0}, '-=0.05');
+        tl.to([this.titleEl, this.barsEls], .3, {height: 0, margin: 0});
         tl.to(['.home'], .3, {width: '50%'}, '-=0.1');
 
         function onTimelineCompleted() {
